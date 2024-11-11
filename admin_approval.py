f0c45ea76c139d55f1cdc81b53fe89b0c69f6ca8
FaiziXd <faiizuxd@gmail.com>
2024-11-11 17:28:48 +0500

    Update admin_approval.py

--- admin_approval.py
+++ admin_approval.py
@@ -13,5 +13,5 @@ def approve_request():
     else:
         return jsonify({"message": "Approval rejected", "status": "rejected"})
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8081)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
